c68727e39529a66315c9729122de8ae5044d90e0
Jorge Roldan <jorge.roldan@gmail.com>
2024-09-17 21:56:30 +0200

    Query execution

---
+++ 03-intermediate-sql/sql/1-selecting-data/2.2-debugging-errors.sql
@@ -0,0 +1,12 @@
+-- Debug this code
+SELECT certification
+FROM films
+LIMIT 5;
+
+-- Debug this code
+SELECT film_id, imdb_score, num_votes
+FROM reviews;
+
+-- Debug this code
+SELECT COUNT(birthdate) AS count_birthdays
+FROM people;
