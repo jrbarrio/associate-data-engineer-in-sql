5a200163f7a4478f226a36499beb443cb62b5759
Jorge Roldan <jorge.roldan@gmail.com>
2024-09-17 22:25:58 +0200

    Multiple criteria

---
+++ 03-intermediate-sql/sql/2-filtering-records/2.2-using-or.sql
@@ -0,0 +1,19 @@
+-- Find the title and year of films from the 1990 or 1999
+SELECT title, release_year
+FROM films
+WHERE release_year = 1990
+    OR release_year = 1999;
+
+SELECT title, release_year
+FROM films
+WHERE (release_year = 1990 OR release_year = 1999)
+-- Add a filter to see only English or Spanish-language films
+	AND (language = 'English' OR language = 'Spanish');
+
+SELECT title, release_year
+FROM films
+WHERE (release_year = 1990 OR release_year = 1999)
+	AND (language = 'English' OR language = 'Spanish')
+-- Filter films with more than $2,000,000 gross
+	AND gross > 2000000;
+
