6e6ee3066038411e4e18135d80f79739f7b4c80d
Jorge Roldan <jorge.roldan@gmail.com>
2024-09-18 21:49:58 +0200

    Sorting results

---
+++ 03-intermediate-sql/sql/4-sorting-and-grouping/1.2-sorting-single-fields.sql
@@ -0,0 +1,9 @@
+-- Select name from people and sort alphabetically
+SELECT name
+FROM people
+ORDER BY name;
+
+-- Select the title and duration from longest to shortest film
+SELECT title, duration
+FROM films
+ORDER BY duration DESC;
---
+++ 03-intermediate-sql/sql/4-sorting-and-grouping/1.3-sorting-multiple-fields.sql
@@ -0,0 +1,9 @@
+-- Select the release year, duration, and title sorted by release year and duration
+SELECT release_year, duration, title
+FROM films
+ORDER BY release_year, duration;
+
+-- Select the certification, release year, and title sorted by certification and release year
+SELECT certification, release_year, title
+FROM films
+ORDER BY certification, release_year DESC;
