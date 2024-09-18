1de475e5f3ddbb3cef6eaca070ebc8d90bff74dd
Jorge Roldan <jorge.roldan@gmail.com>
2024-09-18 22:17:46 +0200

    Filtering grouped data

---
+++ 03-intermediate-sql/sql/4-sorting-and-grouping/3.3-all-together-now.sql
@@ -0,0 +1,27 @@
+-- Select the release_year for films released after 1990 grouped by year
+SELECT release_year
+FROM films
+WHERE release_year > 1990
+GROUP BY release_year;
+
+-- Modify the query to also list the average budget and average gross
+SELECT release_year, AVG(budget) AS avg_budget, AVG(gross) AS avg_gross
+FROM films
+WHERE release_year > 1990
+GROUP BY release_year;
+
+SELECT release_year, AVG(budget) AS avg_budget, AVG(gross) AS avg_gross
+FROM films
+WHERE release_year > 1990
+GROUP BY release_year
+-- Modify the query to see only years with an avg_budget of more than 60 million
+HAVING AVG(budget) > 60000000;
+
+SELECT release_year, AVG(budget) AS avg_budget, AVG(gross) AS avg_gross
+FROM films
+WHERE release_year > 1990
+GROUP BY release_year
+HAVING AVG(budget) > 60000000
+-- Order the results from highest to lowest average gross and limit to one
+ORDER BY avg_gross DESC
+LIMIT 1;
