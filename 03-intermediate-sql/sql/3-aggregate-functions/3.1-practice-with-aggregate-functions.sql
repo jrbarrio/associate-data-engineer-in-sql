d70326090ce8e0c2af243942bc07053a6aa2b37a
Jorge Roldan <jorge.roldan@gmail.com>
2024-09-18 20:31:00 +0200

    Summarizing data

---
+++ 03-intermediate-sql/sql/3-aggregate-functions/3.1-practice-with-aggregate-functions.sql
@@ -0,0 +1,15 @@
+-- Query the sum of film durations
+SELECT SUM(duration) AS total_duration
+FROM films;
+
+-- Calculate the average duration of all films
+SELECT AVG(duration) AS average_duration
+FROM films;
+
+-- Find the latest release_year
+SELECT MAX(release_year) AS latest_year
+FROM films;
+
+-- Find the duration of the shortest film
+SELECT MIN(duration) AS shortest_film
+FROM films;
