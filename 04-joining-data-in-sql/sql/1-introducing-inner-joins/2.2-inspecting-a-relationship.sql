2ebff661d424b278c84f29999104c02d1c215de4
Jorge Roldan <jorge.roldan@gmail.com>
2024-09-21 14:59:29 +0200

    Defining relationships

---
+++ 04-joining-data-in-sql/sql/1-introducing-inner-joins/2.2-inspecting-a-relationship.sql
@@ -0,0 +1,23 @@
+-- Select country (aliased) from countries
+SELECT name AS country
+FROM countries;
+
+-- Select country and language names (aliased)
+SELECT c.name AS country, l.name AS language
+-- From countries (aliased)
+FROM countries AS c
+-- Join to languages (aliased)
+INNER JOIN languages AS l
+-- Use code as the joining field with the USING keyword
+USING(code);
+
+-- Select country and language name (aliased)
+SELECT c.name AS country, l.name AS language
+-- From countries (aliased)
+FROM countries AS c
+-- Join to languages (aliased)
+INNER JOIN languages AS l
+-- Use code as the joining field with the USING keyword
+USING(code)
+-- Filter for the Bhojpuri language
+WHERE l.name = 'Bhojpuri';
